b348d476d4ecca6f4633750f22828fc9d7288062
Jefferyjasmin <Jefferyjasmin1@gmail.com>
2020-11-04 07:07:06 +0000

    staring

---
+++ src/js/component/functions.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const No_name = () => {
+	//for new todo
+	const [userName, setUsername] = useState({});
+	//for our previous todos
+	const [budget, setBudget] = useState([
+		// { todo: "todo 1" },
+	]);
+
+
+    newBudget = <option value="4">Date Night</option>
